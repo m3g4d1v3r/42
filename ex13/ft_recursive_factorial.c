ebca13f7055b8659fb59c700ba3ecc85a8a463c5
Marlon Marçal <msubtil-@student.42sp.org.br>
2022-03-25 16:44:44 +0100

    Exercise 13

---
+++ ex13/ft_recursive_factorial.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msubtil- <msubtil-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/24 04:11:25 by msubtil-          #+#    #+#             */
+/*   Updated: 2022/03/25 16:44:05 by msubtil-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	else if (nb == 0 || nb == 1)
+		return (1);
+	return (nb * ft_recursive_factorial(nb - 1));
+}
