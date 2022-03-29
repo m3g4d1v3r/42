dc3dcaaf9c019092950e9db876d7f3362694421a
Marlon Marçal <msubtil-@student.42sp.org.br>
2022-03-29 20:15:37 +0200

    Modify ex26

--- ex26/ft_count_if.c
+++ ex26/ft_count_if.c
@@ -6,7 +6,7 @@
 /*   By: msubtil- <msubtil-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 03:54:44 by msubtil-          #+#    #+#             */
-/*   Updated: 2022/03/28 04:03:50 by msubtil-         ###   ########.fr       */
+/*   Updated: 2022/03/29 19:53:00 by msubtil-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ int	ft_count_if(char **tab, int (*f)(char*))
 	int	acc;
 
 	acc = 0;
-	while (*tab != (char *) 0)
+	while (*tab)
 	{
 		acc += f(*tab);
 		tab++;
